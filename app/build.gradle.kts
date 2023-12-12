347acdf5aa325de3670d71b9d0af0530756cc248
AtlantisRedStar <atlantisredstar@gmail.com>
2023-12-12 17:33:59 +0200

    Added Testing Units

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+
 }
 
 android {
@@ -30,10 +31,19 @@ android {
 
 dependencies {
 
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test:rules:1.4.0")
+    androidTestImplementation ("org.mockito:mockito-core:3.12.4")
+    testImplementation ("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    testImplementation ("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("org.mockito:mockito-android:3.12.4")
+
 }
